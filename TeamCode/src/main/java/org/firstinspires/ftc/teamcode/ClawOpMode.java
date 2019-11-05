69966324f86a1f2abd593525c97973ffefd14427
Devildogs11206 <devildogs11206@gmail.com>
2019-11-05 11:21:40 -0600

    Adding an opmode for the claw, just to open and close it. It is called ClawOpMode.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ClawOpMode.java
@@ -0,0 +1,54 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.Servo;
+
+public class ClawOpMode extends LinearOpMode {
+
+    RobotHardware robot = new RobotHardware();
+
+    private Servo claw_0 = null;
+
+    //double          clawOffset      = 0;                       // Servo mid position
+    //final double    CLAW_SPEED      = 0.02 ;                   // sets rate to move servo
+
+    @Override
+    public void runOpMode(){
+
+        claw_0 = hardwareMap.get(Servo.class,"claw_0");
+
+        waitForStart();
+
+        while(opModeIsActive()){
+
+            if(gamepad1.a){claw_0.setPosition(1);}
+            if(gamepad1.b){claw_0.setPosition(0.7);}
+
+            /*
+            // Use gamepad left & right Bumpers to open and close the claw
+            if (gamepad1.right_bumper)
+                clawOffset += CLAW_SPEED;
+            else if (gamepad1.left_bumper)
+                clawOffset -= CLAW_SPEED;
+
+
+            // Move both servos to new position.  Assume servos are mirror image of each other.
+            clawOffset = Range.clip(clawOffset, -0.5, 0.5);
+            robot.leftClaw.setPosition(robot.MID_SERVO + clawOffset);
+            robot.rightClaw.setPosition(robot.MID_SERVO - clawOffset);
+
+
+            // Use gamepad buttons to move arm up (Y) and down (A)
+            if (gamepad1.y)
+                robot.leftArm.setPower(robot.ARM_UP_POWER);
+            else if (gamepad1.a)
+                robot.leftArm.setPower(robot.ARM_DOWN_POWER);
+            else
+                robot.leftArm.setPower(0.0);
+
+                */
+
+        }
+
+    }
+}
